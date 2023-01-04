24b226763ed029bde1b497c007b13a820f7b419e
Ethanleewc <ethanleewc@gmail.com>
2023-01-04 23:42:23 +0800

    first commit for day12-workshop

---
+++ src/main/java/com/example/day12workshop/models/Generate.java
@@ -0,0 +1,14 @@
+package com.example.day12workshop.models;
+
+public class Generate {
+    private int numberVal;
+
+    public int getNumberVal() {
+        return numberVal;
+    }
+
+    public void setNumberVal(int numberVal) {
+        this.numberVal = numberVal;
+    }
+    
+}
